01a7d0e93f5c940bcfbba4517e3c1622217ef42e
gouravsiwach <136164754+gouravsiwach@users.noreply.github.com>
2023-11-03 18:38:59 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const uuid = require("uuid");
 
 const userSchema = new mongoose.Schema({
+  uuid: String,
   username: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
